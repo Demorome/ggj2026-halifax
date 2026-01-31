4cd608aeef0243a0ee535fe07d55167f6866f672
demorome <demoromestuff@gmail.com>
2026-01-31 17:43:24 -0400

    add EnemyFieldOfView to generate view-cone mesh, add CodeMonkey utilities cuz why not

---
+++ GJ2026/Assets/scripts/EnemyFieldOfView.cs
@@ -0,0 +1,116 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class EnemyFieldOfView : MonoBehaviour
+{
+    // True if the enemy found the player.
+    public bool HasSeenPlayer { get; private set; } = false;
+
+    [SerializeField] private LayerMask layerMask;
+    private Mesh mesh;
+    public float fov = 90f;
+    public float viewDistance = 50f;
+    private Vector3 origin;
+    private float startingAngle;
+
+    private void Start() {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        origin = Vector3.zero;
+    }
+
+    private void LateUpdate() {
+        int rayCount = 50;
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        HashSet<GameObject> hitObjects = new();
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++) {
+            Vector3 vertex;
+            var vectorFromAngle = UtilsClass.GetVectorFromAngle(angle);
+            bool hit = Physics.Raycast(
+                origin,
+                vectorFromAngle,
+                out RaycastHit hitInfo,
+                viewDistance,
+                layerMask);
+
+            if (!hit || hitInfo.collider == null) {
+                // No hit
+                vertex = origin + vectorFromAngle * viewDistance;
+            } else {
+                // Hit object
+                vertex = hitInfo.point;
+
+                var hitObject = hitInfo.transform.gameObject;
+                hitObjects.Add(hitObject);
+            }
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0) {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
+
+        foreach (var hitObject in hitObjects)
+        {
+            if (hitObject.tag == "Player")
+            {
+                HasSeenPlayer = true;
+                break;
+            }
+        }
+    }
+
+    public void SetOrigin(Vector3 origin) {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection) {
+        startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) + fov / 2f;
+    }
+
+    public void SetFoV(float fov) {
+        this.fov = fov;
+    }
+
+    public void SetViewDistance(float viewDistance) {
+        this.viewDistance = viewDistance;
+    }
+
+}
