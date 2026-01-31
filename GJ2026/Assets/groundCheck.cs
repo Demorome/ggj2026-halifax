94efbd89919b5542470db1ab728165c2319ec497
Kyle Wong <ky607453@dal.ca>
2026-01-30 20:10:44 -0400

    First commit

---
+++ GJ2026/Assets/groundCheck.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class groundCheck : MonoBehaviour
+{
+    public GameObject playerBody;
+    public Collider Collider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //if you exit as in leave the collision it checks if the object you left collision with was environment and if so you cant jump again
+    public void OnTriggerExit(Collider collObj)
+    {
+        if (collObj.tag == "environment")
+        {
+            //Debug.Log("exit: " + collObj);
+            playerBody.GetComponent<playerController>().grounded = false;
+        }
+    }
+    //collObj = colliding object
+    public void OnTriggerEnter(Collider collObj)
+    {
+        if (collObj.tag == "environment")
+        {
+            //Debug.Log("Enter: " + collObj);
+            //Debug.Log(collObj.tag);
+            playerBody.GetComponent<playerController>().grounded = true;
+        }
+    }
+
+    
+}
+
---
+++ GJ2026/Assets/playerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+ public GameObject groundCheck;
+    public bool grounded = true;
+    public float moveSpeed = 10f;
+    public float jumpHeight = 100f;
+    public Rigidbody rb;
+
+    private Vector3 jump =  Vector3.zero;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if the groundchecker is touching or not
+        if (grounded == true)
+        {
+
+            //down means it can only be pressed not held down note to self think about jetpack for holding down
+            if (Input.GetKeyDown("space"))
+            {
+                Debug.Log("jump");
+                //adding force up
+                jump.y = jumpHeight * Time.deltaTime * 1000;
+                rb.AddForce(transform.up * jumpHeight);
+
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+
+        //rotating based on mouse position
+        //rb.rotation =  Quaternion.Euler(-Input.mousePosition.y , Input.mousePosition.x, 0);
+
+        //move based on axis input
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed, Space.Self);
+
+
+        //on click do a swing
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("swing");
+        }
+        
+
+    }
+    //knock back method for if hit
+    public void knockback(Quaternion rotation, float knockback)
+    {
+        Debug.Log("knockback");
+        //send them back from where enemy is facing
+        rb.AddForce(rotation * Vector3.back * -knockback);
+    }
+}
