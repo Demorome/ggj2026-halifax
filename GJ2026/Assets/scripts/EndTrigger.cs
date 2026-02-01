6fa0f93ebc4057921abc22448fb05b226a97fb9a
demorome <demoromestuff@gmail.com>
2026-02-01 09:53:22 -0400

    don't need gameManager member here

--- GJ2026/Assets/scripts/EndTrigger.cs
+++ GJ2026/Assets/scripts/EndTrigger.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
-    public GameManager gameManager;
     private void OnTriggerEnter()
     {
         GameManager.Instance.LoadNextScene();
