2ae175c65c9571fd88b68b69abc21873a90c1607
demorome <demoromestuff@gmail.com>
2026-03-28 21:15:33 -0300

    add WIP objective UI display.
    Works per-level, giving full control over what the level's objectives are, and allowing for dynamic objective changes depending on player's current form.

--- GJ2026/Assets/scripts/Components/ActorTypeComponent.cs
+++ GJ2026/Assets/scripts/Components/ActorTypeComponent.cs
@@ -9,6 +9,7 @@ namespace Components
         Cat,
         Dog,
         Human,
+        Unknown
     }
 
     /// <summary>
---
+++ GJ2026/Assets/scripts/LevelObjectives/BackyardLevelObjectives.cs
@@ -0,0 +1,119 @@
+using Components;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace LevelObjectives
+{
+    // Sets the script to be executed later than all default scripts
+    // This is helpful for UI, since other things may need to be initialized before setting the UI
+    [DefaultExecutionOrder(1000)]
+    public class BackyardLevelObjectives : MonoBehaviour
+    {
+        private UIDocument uiDocument;
+        private TextElement uiText;
+
+        /// <summary>
+        /// Can be 0, to represent a boolean objective
+        /// which doesn't require point accumulation.
+        /// </summary>
+        private int progressRequired;
+        private int currentProgress;
+
+        private int currentCollectedCheese = 0;
+        [SerializeField]
+        private int requiredCollectedCheese = 3;
+
+        private int currentCollectedDogBones = 0;
+        [SerializeField]
+        private int requiredCollectedDogBones = 1;
+
+        private int currentIntrudersRepelled = 0;
+        [SerializeField]
+        private int requiredIntrudersRepelled = 3;
+
+
+        private ActorType _currentActorType = ActorType.Unknown;
+        private ActorType currentActorType
+        {
+            get => _currentActorType;
+            set
+            {
+                if (_currentActorType != value)
+                {
+                    _currentActorType = value;
+                    OnActorTypeChanged(value);
+                }
+            }
+        }
+
+        private void OnActorTypeChanged(ActorType actorType)
+        {
+            string objectiveMessage;
+            if (actorType == ActorType.PlayerMaskWithoutHost)
+            {
+                objectiveMessage = "Return home without killing any host!";
+            }
+            else if (actorType == ActorType.Mouse)
+            {
+                objectiveMessage = "Collect cheese!";
+                progressRequired = requiredCollectedCheese;
+                currentProgress = currentCollectedCheese;
+            }
+            else if (actorType == ActorType.Cat)
+            {
+                objectiveMessage = "Steal the dog's bone!";
+                progressRequired = requiredCollectedDogBones;
+                currentProgress = currentCollectedDogBones;
+
+            }
+            else if (actorType == ActorType.Dog)
+            {
+                progressRequired = requiredIntrudersRepelled;
+                currentProgress = currentIntrudersRepelled;
+
+                if (currentProgress >= requiredIntrudersRepelled)
+                {
+                    objectiveMessage = "Enter the house!";
+                }
+                else
+                {
+                    objectiveMessage = "Repel intruders from the house!";
+                }
+            }
+            else
+            {
+                objectiveMessage = "ERROR: Unknown objective!";
+            }
+
+            UpdateObjectiveUIString(objectiveMessage);
+        }
+
+        // TODO!!!!!!!!!!!!!!!!!!!!!!
+        private void UpdateObjectiveUIString(string objectiveMsg)
+        {
+            uiText.text = objectiveMsg;
+        }
+
+        private void Start()
+        {
+            uiDocument = GetComponent<UIDocument>();
+            uiText = uiDocument.rootVisualElement.Q<TextElement>("Objective");
+            if (uiText == null)
+            {
+                Debug.LogError("Objective UI Text not found!");
+                return;
+            }
+
+            var currentPlayerForm = CanPossessComponent.CurrentPlayerForm;
+            if (!currentPlayerForm)
+            {
+                // TODO: Set the objective UI to be an error message.
+                Debug.LogError("Player-controlled object not found!");
+                return;
+            }
+            var actorType = currentPlayerForm.GetComponent<ActorTypeComponent>().actorType;
+
+            currentActorType = actorType;
+        }
+    }
+}
